38cbe35fce9cdb1509009917dae31cd8f8f31a11
Casey <cfeikema@usf.edu>
2021-04-17 18:54:59 -0400

    Added average user bpm and color for metronome btn

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,23 @@
 <template>
-    <div id="app">
-        <h1>{{title}}</h1>
-        <Metronome/>
-        <button class="tempo-button" @mousedown="updateClickTime">
-            <div class="tempo-outline"></div>
-            <div class="container">
-                <div class="user-tempo">{{buttonClickBPM}}</div>
-                <div class="tempo-message">{{tempoMessage}}</div>
-            </div>
-        </button>
+  <div id="app" class="main-container">
+    <div class="title">
+      <h1>{{title}}</h1>
     </div>
+    <div class="metronome">
+      <Metronome/>
+    </div>
+    <button class="tempo-button" @mousedown="updateClickTime">
+      <div class="tempo-outline"></div>
+      <div class="container">
+        <div class="user-tempo">{{buttonClickBPM}}</div>
+        <div class="tempo-message">{{tempoMessage}}</div>
+      </div>
+    </button>
+    <div class="statistics">
+        <h3>Your Average BPM: {{averageUserBPM}}</h3>
+        <button class="button reset" @click="resetStats" >Reset Statistics</button>
+    </div>
+  </div>
 </template>
 
 <script>
@@ -26,6 +34,7 @@ export default {
         tempoMessage: "Click me!",
         buttonClickBPM: (0).toFixed(1), // Without toFixed(1), buttonClickBPM will initialize as 0 and then change to 0.0 at the first button press
         clickTime: 0,  
+        allUserBPMs: []
     }
   },
   watch: {
@@ -37,13 +46,30 @@ export default {
           flush: 'post'
       }
   },
-  computed: {},
+  computed: {
+    averageUserBPM(){
+      let sum = 0;
+      let average = 0;
+      if(this.allUserBPMs.length > 1){
+        this.allUserBPMs.forEach(function(userBPM){
+          sum += userBPM;
+        })
+        average = (sum/(this.allUserBPMs.length))
+      }
+      return average.toFixed(1);
+    }
+  },
   methods: {
     updateClickTime(){ 
         this.clickTime = new Date().getTime(); 
     },
     calculateBPM(newTime, oldTime){
-        return (60/((newTime-oldTime)/1000)).toFixed(1);
+        let BPM = (60/((newTime-oldTime)/1000));
+        if(BPM > 1) this.allUserBPMs.push(BPM); // less than 1 is an outlier that contaminates the array
+        return BPM.toFixed(1);
+    },
+    resetStats(){
+      this.allUserBPMs = [];
     }
   }
 }
@@ -59,26 +85,43 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #fff;
-  margin-top: 60px;
+}
+
+.main-container{
+  display: grid;
+  place-items: center;
+  grid-template-columns: 1fr 1fr 1fr;
+  grid-template-rows: auto;
+  grid-template-areas: 
+    "header header header"
+    "main-top main-top main-top"
+    "main main main"
+    "footer footer footer";
+}
+
+.title{
+  grid-area: header;
+}
+
+.metronome{
+  grid-area: main-top;
 }
 
 .tempo-button{
-    position: absolute;
-    top: 50%;
-    left: 50%;
-    width: 220px;
-    height: 220px;
-    border-radius: 50%;
-    border: none;
-    outline: none;
-    user-select: none;
-    margin-right: -50%;
-    transform: translate(-50%, -50%);
-    transition: all ease-out .15s;
+  grid-area: main;
+  position: absolute;
+  top: 40%;
+  width: 220px;
+  height: 220px;
+  border-radius: 50%;
+  border: none;
+  outline: none;
+  user-select: none;
+  transition: all ease-out .15s;
 }
 
 .tempo-button:hover{
-    filter: brightness(0.9);
+  filter: brightness(0.9);
 }
 
 .tempo-outline{
@@ -87,33 +130,62 @@ export default {
 	left: 0;
 	bottom: 0;
 	right: 0;
-    background: linear-gradient(to right bottom, #38d18c, #ffffff);
+  background: linear-gradient(to right bottom, #38d18c, #ffffff);
 	cursor: pointer;
 	border-radius: 50%;
 }
+
 .container{
-    position: absolute;
-    top: 4%;
-    left: 4%;
-    bottom: 4%;
-    right: 4%;
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    flex-direction: column;
-    border-radius: 50%;
+  position: absolute;
+  top: 4%;
+  left: 4%;
+  bottom: 4%;
+  right: 4%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+  border-radius: 50%;
 }
+
 .user-tempo{
-    color: white;
-    font-size: 4rem;
-    font-family: 'Segoe UI', Tahoma, Geneva, Verdana, sans-serif;
+  color: white;
+  font-size: 4rem;
+  font-family: 'Segoe UI', Tahoma, Geneva, Verdana, sans-serif;
 }
+
 .tempo-message{
-    color: white;
-    font-size: 1rem;
-    font-weight: bold;
-    font-family: 'Segoe UI', Tahoma, Geneva, Verdana, sans-serif;
+  color: white;
+  font-size: 1rem;
+  font-weight: bold;
+  font-family: 'Segoe UI', Tahoma, Geneva, Verdana, sans-serif;
 }
 
+.statistics{
+  grid-area: footer;
+  display: inline-grid;
+  grid-auto-flow: column;
+  justify-items: center;
+  align-items: center;
+}
 
+.button{
+  border: 4px solid white;
+  border-radius: 4px;
+  background-color: white;
+  display: inline-block; 
+  text-decoration: none;
+  font-family: inherit;
+  font-size: 1rem;
+  font-weight: 600;
+  cursor: pointer;
+  text-align: center;
+  -webkit-appearance: none;
+  -moz-appearance: none;
+  outline: none;
+}
+
+.reset{
+  margin-left: 10px;
+}
 </style>
--- src/components/Metronome.vue
+++ src/components/Metronome.vue
@@ -1,7 +1,7 @@
 <template>
     <div>
         <div id="metronomeInputs">
-            <button class="on-off" @click="metronome.toggleOnOff()">{{buttonDoes}} Metronome</button>
+            <button class="toggle" :class="{'on': !metronome.keepingBeat, 'off': metronome.keepingBeat}" @click="metronome.toggleOnOff()">{{buttonDoes}} Metronome</button>
             <h3><input v-model.number="metronome.bpm" class="input-number" type="number" min="20" max="160" 
                 size="2"/> BPM
             </h3>
@@ -83,28 +83,34 @@ export default {
   justify-items: center;
   align-items: center;
 }
-.on-off {
-    background: white;
+.toggle {
+    
     border-top-left-radius: 4px;
     border-bottom-left-radius: 4px;
     display: inline-block;
-    border: 4px solid white;
-    margin-right: 2px;
+    
     text-decoration: none;
-    color: #2c3035;
     font-family: inherit;
     font-size: 1rem;
+    font-weight: 600;
+    color: white;
     cursor: pointer;
     text-align: center;
     -webkit-appearance: none;
     -moz-appearance: none;
     outline: none;
 }
-
+.on{
+  background: #38d18c;
+  border: 4px solid #38d18c;
+}
+.off{
+  background: #d13838;
+  border: 4px solid #d13838;
+}
 .input-number{
     font-size: 16px;
     font-family: inherit;
-    color: #2c3035;
     background-color: white;
     border: 4px solid white;
     padding-left: 6px;
