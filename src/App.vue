506ae52c75982dc43a27f120b5a1c4b8ac3e5fa0
Casey <cfeikema@usf.edu>
2021-04-17 15:29:14 -0400

    init commit

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,119 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+    <div id="app">
+        <h1>{{title}}</h1>
+        <Metronome/>
+        <button class="tempo-button" @mousedown="updateClickTime">
+            <div class="tempo-outline"></div>
+            <div class="container">
+                <div class="user-tempo">{{buttonClickBPM}}</div>
+                <div class="tempo-message">{{tempoMessage}}</div>
+            </div>
+        </button>
+    </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Metronome from './components/Metronome.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+      Metronome
+  },
+  data() {
+    return {
+        title: "Follow the beat!",
+        tempoMessage: "Click me!",
+        buttonClickBPM: (0).toFixed(1), // Without toFixed(1), buttonClickBPM will initialize as 0 and then change to 0.0 at the first button press
+        clickTime: 0,  
+    }
+  },
+  watch: {
+      clickTime: {
+          handler(newTime, oldTime){
+              this.tempoMessage = "BPM";
+              this.buttonClickBPM = this.calculateBPM(newTime, oldTime);
+          },
+          flush: 'post'
+      }
+  },
+  computed: {},
+  methods: {
+    updateClickTime(){ 
+        this.clickTime = new Date().getTime(); 
+    },
+    calculateBPM(newTime, oldTime){
+        return (60/((newTime-oldTime)/1000)).toFixed(1);
+    }
   }
 }
 </script>
 
 <style>
+*{
+  background-color: #2c3035;
+}
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
+  font-family: 'Segoe UI', Tahoma, Geneva, Verdana, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
+  color: #fff;
   margin-top: 60px;
 }
+
+.tempo-button{
+    position: absolute;
+    top: 50%;
+    left: 50%;
+    width: 220px;
+    height: 220px;
+    border-radius: 50%;
+    border: none;
+    outline: none;
+    user-select: none;
+    margin-right: -50%;
+    transform: translate(-50%, -50%);
+    transition: all ease-out .15s;
+}
+
+.tempo-button:hover{
+    filter: brightness(0.9);
+}
+
+.tempo-outline{
+	position: absolute;
+	top: 0;
+	left: 0;
+	bottom: 0;
+	right: 0;
+    background: linear-gradient(to right bottom, #38d18c, #ffffff);
+	cursor: pointer;
+	border-radius: 50%;
+}
+.container{
+    position: absolute;
+    top: 4%;
+    left: 4%;
+    bottom: 4%;
+    right: 4%;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    flex-direction: column;
+    border-radius: 50%;
+}
+.user-tempo{
+    color: white;
+    font-size: 4rem;
+    font-family: 'Segoe UI', Tahoma, Geneva, Verdana, sans-serif;
+}
+.tempo-message{
+    color: white;
+    font-size: 1rem;
+    font-weight: bold;
+    font-family: 'Segoe UI', Tahoma, Geneva, Verdana, sans-serif;
+}
+
+
 </style>
---
+++ src/components/Metronome.vue
@@ -0,0 +1,103 @@
+<template>
+    <div>
+        <div id="metronomeInputs">
+            <h3>Metronome BPM:  <input v-model.number="metronome.bpm" class="input-number" type="number" min="20" max="160" 
+                size="3"/>
+            </h3>
+        </div>
+        <button class="on-off" @click="metronome.toggleOnOff()">{{buttonDoes}} Metronome</button>
+    </div>
+</template>
+
+<script>
+
+const soundFilePath = require("@/assets/drumSticks.mp3");
+
+class Metronome {
+    constructor(bpm) {
+        this.bpm = bpm; 
+        this.beatSound = new Audio(soundFilePath);
+        this.keepingBeat = false;
+    }
+    start(){
+        let msBetweenBeats = (60/this.bpm)*1000;
+        this.beatSound.play(); // Play an initial beat
+        this.intervalID = setInterval(()=>(this.beatSound.play()), msBetweenBeats);
+        this.keepingBeat = true;
+    }
+    stop(){
+        this.keepingBeat = false;
+        clearInterval(this.intervalID);
+    }
+    restart(){
+        this.stop();
+        this.start();
+    }
+    toggleOnOff(){
+        if(this.keepingBeat){
+            this.stop();
+        } else{
+            this.start();
+        }
+    }
+}
+
+export default {
+  name: 'Metronome',
+  props: {
+      initialBPM: {
+          type: Number,
+          default: 60
+      }
+  },
+  data() {
+    return {
+        metronome: new Metronome(this.initialBPM)
+    }
+  },
+  watch: {
+      'metronome.bpm': {
+          handler(){
+              if(this.metronome.keepingBeat)
+                this.metronome.restart();
+          }
+      }
+  },
+  computed: {
+      buttonDoes(){
+          if(this.metronome.keepingBeat){
+              return "Stop";
+          } else{
+              return "Start";
+          }
+      }
+  }
+}
+</script>
+
+<style>
+.on-off {
+    background: white;
+    border-radius: 4px;
+    display: inline-block;
+    border: none;
+    padding: 0.5rem 0.75rem;
+    text-decoration: none;
+    color: #2c3035;
+    font-family: inherit;
+    font-size: 1rem;
+    cursor: pointer;
+    text-align: center;
+    -webkit-appearance: none;
+    -moz-appearance: none;
+}
+
+.input-number{
+    font-size: 16px;
+    font-family: inherit;
+    color: #2c3035;
+    background-color: white;
+    border: 4px solid white;
+    border-radius: 4px;    
+}
+</style>
